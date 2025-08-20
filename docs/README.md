cb44d185c0ae83c202b8ee9e2fe4999574489624
jlismee <e1355121@u.nus.edu>
2025-08-20 14:55:45 +0800

    Rename, Greet, Exit Renamed Duke to Bambam and implemented the skeletal version of it

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Bambam User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Bambam.java
@@ -0,0 +1,8 @@
+public class Bambam {
+    public static void main(String[] args) {
+        String chatBotName = "Bambam";
+        System.out.println("Hello! I'm " + chatBotName + "\n" +
+                "What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
