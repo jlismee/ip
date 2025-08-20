3b52ba954150a336cfe6152b38f9b55098d53f27
jlismee <e1355121@u.nus.edu>
2025-08-20 17:27:54 +0800

    Created a TaskList class this class involves functions related to the task list

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,23 @@
+public class TaskList {
+    private String[] taskList;
+    private int taskCounter;
+
+    public TaskList() {
+        this.taskList = new String[100];
+        this.taskCounter = 0;
+    }
+
+    // function to add task to the task list
+    public void addTaskToList(String task) {
+        taskList[taskCounter] = task;
+        taskCounter++;
+    }
+
+    // function to print list of tasks
+    public void printList() {
+        for (int i = 0; i < taskCounter; i++) {
+            int count = i + 1;
+            System.out.println(count + ". " + taskList[i]);
+        }
+    }
+}
