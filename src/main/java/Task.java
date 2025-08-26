9b073965de97f3eea6947669cf9eb5e00344ebd4
jlismee <e1355121@u.nus.edu>
2025-08-26 12:32:32 +0800

    Add date and time features Able to pass date and time in string type and return in LocalDateTime type

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,43 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Task {
     private String taskDescription;
     private boolean isDone;
+    private static final DateTimeFormatter DATE_TIME_INPUT_FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter DATE_INPUT_FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter DATE_TIME_OUTPUT_FORMAT =
+            DateTimeFormatter.ofPattern("MMM d yyyy HHmm");
+    private static final DateTimeFormatter DATE_OUTPUT_FORMAT =
+            DateTimeFormatter.ofPattern("MMM d yyyy");
 
     public Task(String taskDescription) {
         this.taskDescription = taskDescription;
         this.isDone = false;
+    }
+
+    // Returns dates and times in LocalDateTime
+    public LocalDateTime getLocalDateTime(String dateTime) {
+        String[] dateTimeDetails = dateTime.split(" ", 2);
+        if (dateTimeDetails.length == 2) {
+            return LocalDateTime.parse(dateTime, DATE_TIME_INPUT_FORMATTER);
+        } else {
+            LocalDate deadlineDate = LocalDate.parse(dateTime, DATE_INPUT_FORMATTER);
+            return deadlineDate.atStartOfDay();
+        }
+    }
+
+    // Returns LocalDateTime as Strings
+    public String printLocalDateTime(LocalDateTime dateTime) {
+        if (dateTime.toLocalTime().equals(LocalTime.MIDNIGHT)) {
+            return dateTime.format(DATE_OUTPUT_FORMAT);
+        } else {
+            return dateTime.format(DATE_TIME_OUTPUT_FORMAT);
+        }
     }
 
     // function to obtain the string to print depending on whether the task is completed
